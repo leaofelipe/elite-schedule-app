033fe6db740c9bb332c1c3aa58d83a019e792646
Felipe Leão <felipe@felipeleao.com>
2017-10-17 15:45:07 -0200

    Create navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { PageOne } from '../pages/page1/page1';
 
 @Component({
   templateUrl: 'app.html'
@@ -23,6 +24,7 @@ export class MyApp {
     this.pages = [
       { title: 'Home', component: HomePage },
       { title: 'List', component: ListPage }
+      { title: 'Page 1', component: PageOne }
     ];
 
   }
