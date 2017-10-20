28a50d35f0f8c42b65fe9fb8554febfd34b4e2d0
Felipe Leão <felipe@felipeleao.com>
2017-10-20 14:31:31 -0200

    Implement dividers

--- src/pages/MyTeams/myTeams.ts
+++ src/pages/MyTeams/myTeams.ts
@@ -1,17 +1,46 @@
 import { Component } from '@angular/core'
-import { NavController } from 'ionic-angular'
+import { LoadingController, NavController } from 'ionic-angular'
 
 import { TournamentsPage } from '../Tournaments/tournaments'
+import { TeamHomePage } from '../TeamHome/teamHome'
+import { EliteApi } from '../../app/shared/elite-api.service'
 
 @Component ({
   selector: 'page-myteams',
   templateUrl: 'myTeams.html'
 })
 
-export class MyTeamsPage {
-  constructor (private nav: NavController) {}
+export class MyTeamsPage {  
+  favorites = [
+    {
+      team: {
+        id: 1,
+        name: 'Team 1',
+        coach: 'Felipe',
+        tournamentId: '1',
+        tournamentName: 'T1'
+      }
+    }
+  ]
+
+  constructor (
+    private nav: NavController,
+    private loadingController: LoadingController,
+    private eliteApi: EliteApi
+  ) {}
 
   goToTournaments () {
     this.nav.push(TournamentsPage);
+  }
+
+  favoriteTapped($event, favorite) {
+    let loader = this.loadingController.create({
+      content: 'Getting data...',
+      dismissOnPageChange: true
+    })
+
+    loader.present()
+    this.eliteApi.getTournamentData(favorite.tournamentId)
+    .subscribe(t => this.nav.push(TeamHomePage, favorite.team))
   }
 }
--- src/pages/Teams/teams.ts
+++ src/pages/Teams/teams.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core'
-import { NavController, NavParams } from 'ionic-angular'
+import { LoadingController, NavController, NavParams } from 'ionic-angular'
+import * as _ from 'lodash'
 
 import {TeamHomePage} from '../TeamHome/teamHome'
 import { EliteApi } from '../../app/shared/elite-api.service'
@@ -10,18 +11,42 @@ import { EliteApi } from '../../app/shared/elite-api.service'
 })
 
 export class TeamsPage {
+  private allTeams : any;
+  private allTeamDivisions: any;
   teams = []
-  constructor (private nav: NavController, private navParams: NavParams, private eliteApi: EliteApi) {}
+  constructor (
+    private nav: NavController,
+    private navParams: NavParams,
+    private eliteApi: EliteApi,
+    private loadingController: LoadingController
+  ) {}
 
   itemTapped ($event, team) {
     this.nav.push(TeamHomePage, team)
   }
 
   ionViewDidLoad () {
-    let selectedTourney = this.navParams.data
-    this.eliteApi.getTournamentData(selectedTourney.id)
-    .subscribe(data => {
-      this.teams = data.teams
+    let selectedTourney = this.navParams.data;
+    let loader = this.loadingController.create({
+      content: 'Getting data...'
+    })
+    loader.present()
+    .then(() => {
+      let selectedTourney = this.navParams.data
+      this.eliteApi.getTournamentData(selectedTourney.id)
+      .subscribe(data => {
+        this.allTeams = data.teams;
+        this.allTeamDivisions = _
+        .chain(data.teams)
+        .groupBy('division')
+        .toPairs()
+        .map(item => _.zipObject(
+          ['divisionName', 'divisionTeams'], item
+        ))
+        .value()
+        this.teams = this.allTeamDivisions;
+        loader.dismiss()
+      })
     })
   }
 }
